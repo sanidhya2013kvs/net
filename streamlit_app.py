66e114c1027663a8a05af66b8d8a875e2d5e5501
sanidhya2013kvs <95016329+sanidhya2013kvs@users.noreply.github.com>
2023-04-05 20:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ if clicked:
   
   a='<audio controls autoplay><source src="'
   type_tiny = pyshorteners.Shortener()
-  short_url = type_tiny.qpsru.short(dict[i])  
+  short_url = type_tiny.tinyurl.short(dict[i])  
   w=short_url
   r='"'
   wet='   type="audio/ogg"></audio>'
