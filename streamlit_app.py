1e721fb89ee02ecbbabd913e1d33f9ca19299ec6
sanidhya2013kvs <95016329+sanidhya2013kvs@users.noreply.github.com>
2023-04-05 10:56:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ if clicked:
   a='<audio controls autoplay><source src='
   w=CWF
   wet='type="audio/ogg"></audio>'
-  audio=a+w+wet
+  #audio=a+w+wet
   #st.markdown(html_string,unsafe_allow_html=True)
 
   #name=audio_file.name
@@ -70,7 +70,8 @@ if clicked:
   st.sidebar.subheader(a)
 
   components.html(html_string)
-  st.sidebar.markdown("<audio id="audio" controls autoplay></audio>",unsafe_allow_html=True)
+  string=f'<audio id="audio" controls autoplay></audio>'
+  st.sidebar.markdown(string,unsafe_allow_html=True)
   st.sidebar.audio(audio_bytes, format='audio/ogg')
 
 
