4bbaef07663cd153bfb723cd39005d62faa1c44b
sanidhya2013kvs <95016329+sanidhya2013kvs@users.noreply.github.com>
2023-04-05 16:39:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ content = """ <a href='#' id=0><img src=https://github.com/sanidhya2013kvs/image
 clicked = click_detector(content)
   
 if clicked:
-  
+  st.clear()
   print("hello")
   print(clicked)
   i=int(clicked)
