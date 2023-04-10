18e1870ed17f48fa4d431261453d234590475acd
metha24353backhod <130365223+metha24353backhod@users.noreply.github.com>
2023-04-10 18:08:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="Geet_sagar",page_icon="🧊")
 image="https://github.com/sanidhya2013kvs/net/blob/main/images/ram%20ram%20jai%20raja%20ram.jpg?raw=true"
 
-st.set_page_config(**PAGE_CONFIG)
+
 # Define your javascript
 with open('./name_songs.txt') as f:
     content = f.readlines()
